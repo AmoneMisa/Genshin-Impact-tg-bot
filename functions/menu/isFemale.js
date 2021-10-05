67bdfb5ec5ef18fcd56c4dfc133b98194f37533c
WhitesLove <kubai.rita2@yandex.ru>
2021-10-05 23:17:07 +0300

    Release version 1.3
    Fix for isFemale function

--- functions/menu/isFemale.js
+++ functions/menu/isFemale.js
@@ -1,8 +1,4 @@
 module.exports = function (text = "") {
     let dictionary = ["девушка", "девочка", "женщина", "жэнщина", "мадам", "она", "миледи", "леди", "ж", "жен", "женский", "девчачий", "не мужской", "не скажу", "не знаю", "би", "трап", "trap"];
-    for (let word of dictionary) {
-        if (text.toLowerCase().includes(word)) {
-            return true;
-        }
-    }
+    return dictionary.includes(text.toLowerCase().trim());
 };
