a798a7d27f4dc130e8c693c27db15ca604996c39
WhitesLove <kubai.rita2@yandex.ru>
2021-10-06 10:04:13 +0300

    Release version 1.4
    Add timer for each user

--- functions/titles/titleMessage.js
+++ functions/titles/titleMessage.js
@@ -1,17 +1,16 @@
 const getTitle = require('./getTitle');
-
-let timerTitleCallback;
+// const {allUsers} = require('../../data');
 
 module.exports = function (session) {
     let newDate = Math.round(new Date().getTime() / 1000);
-    if (!timerTitleCallback || (newDate - timerTitleCallback) >= 0) {
-        timerTitleCallback = Math.round(new Date().getTime() / 1000 + 90);
+    if (!session.timerTitleCallback || (newDate - session.timerTitleCallback) >= 0) {
+        session.timerTitleCallback = Math.round(new Date().getTime() / 1000 + 600);
         return `Сегодня ты, @${session.userChatData.user.username}, ${getTitle(session)}!`;
     } else {
-        if ((timerTitleCallback - newDate) < 60) {
-            return `Команду можно вызывать раз в 1.30 минут. Осталось: ${(timerTitleCallback - newDate)} сек`;
-        } else if ((timerTitleCallback - newDate) > 60) {
-            return `Команду можно вызывать раз в 1.30 минут. Осталось: ${Math.round((timerTitleCallback - newDate) / 60)} мин`;
+        if ((session.timerTitleCallback - newDate) < 60) {
+            return `@${session.userChatData.user.username}, команду можно вызывать раз в 10 минут. Осталось: ${(session.timerTitleCallback - newDate)} сек`;
+        } else if ((session.timerTitleCallback - newDate) > 60) {
+            return `@${session.userChatData.user.username}, команду можно вызывать раз в 10 минут. Осталось: ${Math.round((session.timerTitleCallback - newDate) / 60)} мин`;
         }
     }
 };
