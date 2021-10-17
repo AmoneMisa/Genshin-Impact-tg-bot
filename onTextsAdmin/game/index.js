7bef2f93987ba278b13c011847626792f6eff22e
WhitesLove <kubai.rita2@yandex.ru>
2021-10-17 13:55:00 +0300

    Release version 4.0
    add reset point game admin command

--- onTextsAdmin/game/index.js
+++ onTextsAdmin/game/index.js
@@ -1,7 +1,9 @@
 const sword = require('./sword');
 const chest = require('./chest');
+const point = require('./point');
 
 module.exports = [
     ...sword,
-    ...chest
+    ...chest,
+    ...point
 ];
