62b6710c823787206d7b5c85107a2b784de4d0ba
WhitesLove <kubai.rita2@yandex.ru>
2021-10-20 13:10:23 +0300

    Release version 4.0
    Inc timeout for boss loot message

--- templates/statsTemplate.js
+++ templates/statsTemplate.js
@@ -4,104 +4,132 @@ module.exports = {
             {
                 slot: 1,
                 name: "Взмах меча",
+                description: "Быстрый удар мечом.",
                 effect: "common_attack",
                 cooltime: 0,
                 isSelf: false,
                 isDealDamage: true,
                 isBuff: false,
-                power: 3
+                power: 3,
+                needLvl: 1,
+                cost: 0
             },
             {
                 slot: 2,
                 name: "Ярость вампира",
+                description: "Призыв магии крови: наносит 140% урона врагу, вампиря себе здоровье в количестве равное 5% от нанесённого урона.",
                 effect: "vampire",
                 cooltime: 2,
                 isSelf: false,
                 isDealDamage: true,
                 isBuff: false,
-                power: 5
+                power: 5,
+                needLvl: 5,
+                cost: 500
             },
             {
                 slot: 3,
                 name: "Вожделение паладина",
+                description: "Ты собираешь всю волю в кулак и совершаешь мощный удар, который наносит 350% урона.",
                 effect: "strong_attack",
                 cooltime: 3,
                 isSelf: false,
                 isDealDamage: true,
                 isBuff: false,
-                power: 1
+                power: 1,
+                needLvl: 7,
+                cost: 1500
             }
         ]
     },
     mage: {
-        attack: 4, deffence: 1, criticalChance: 20, criticalDamage: 0, skills: [
+        attack: 4, deffence: 2, criticalChance: 20, criticalDamage: 0, skills: [
             {
                 slot: 1,
                 name: "Выстрел из посоха",
+                description: "Выстрел из посоха нейтральной стихией.",
                 effect: "common_attack",
                 cooltime: 0,
                 isSelf: false,
                 isDealDamage: true,
                 isBuff: false,
-                power: 4
+                power: 4,
+                needLvl: 1,
+                cost: 0
             },
             {
                 slot: 2,
                 name: "Грозовая стужа",
+                description: "Запуск огромного грозового вихря, сопровождающегося ледяными шипами. Наносит 500% урона.",
                 effect: "magic_electro_ice_attack",
                 cooltime: 4,
                 isSelf: false,
                 isDealDamage: true,
                 isBuff: false,
-                power: 8
+                power: 8,
+                needLvl: 5,
+                cost: 5000
             },
             {
                 slot: 3,
                 name: "Лунная тень",
+                description: "Создаёт щит на 45% от хп.",
                 effect: "shield",
                 cooltime: 5,
                 isSelf: true,
                 isDealDamage: false,
                 isBuff: true,
-                power: 1
+                power: 1,
+                needLvl: 8,
+                cost: 1200
             }
         ]
     },
-    priest: {attack: 1, deffence: 5, criticalChance: 5, criticalDamage: 50, skills: [
+    priest: {
+        attack: 1, deffence: 5, criticalChance: 5, criticalDamage: 50, skills: [
             {
                 slot: 1,
                 name: "Удар скипетром",
+                description: "Удар скипетром по болевым точкам врага.",
                 effect: "common_attack",
                 cooltime: 0,
                 isSelf: false,
                 isDealDamage: true,
                 isBuff: false,
-                power: 2
+                power: 2,
+                needLvl: 1,
+                cost: 0
             },
             {
                 slot: 2,
                 name: "Сияние утренней звезды",
-                description: "Призыв святой энергии, которая исцеляет раны. Восстанавливает тебе 200 хп.",
+                description: "Призыв святой энергии, которая исцеляет раны. Восстанавливает тебе 25% хп.",
                 effect: "heal",
                 cooltime: 2,
                 isSelf: true,
                 isDealDamage: false,
                 isBuff: false,
-                power: 1
+                power: 1,
+                needLvl: 3,
+                cost: 300
             },
             {
                 slot: 3,
                 name: "Казнь святых",
-                description: "Меч из чистой энергии света поражает твоего врага, нанося ему 200% урона и создавая на тебе щит, который поглощает 500 урона",
+                description: "Меч из чистой энергии света поражает твоего врага, нанося ему 200% урона и создавая на тебе щит, который поглощает 75% урона",
                 effect: "magic_holy",
                 cooltime: 6,
                 isSelf: false,
                 isDealDamage: true,
                 isBuff: true,
-                power: 8
+                power: 8,
+                needLvl: 6,
+                cost: 2000
             }
-        ]},
-    archer: {attack: 5, deffence: 1, criticalChance: 25, criticalDamage: 0, skills: [
+        ]
+    },
+    archer: {
+        attack: 5, deffence: 0, criticalChance: 25, criticalDamage: 20, skills: [
             {
                 slot: 1,
                 name: "Точный выстрел",
@@ -111,7 +139,9 @@ module.exports = {
                 isSelf: false,
                 isDealDamage: true,
                 isBuff: false,
-                power: 5
+                power: 5,
+                needLvl: 1,
+                cost: 0
             },
             {
                 slot: 2,
@@ -124,6 +154,7 @@ module.exports = {
                 isBuff: false,
                 power: 8,
                 needlvl: 7,
+                cost: 3500
             },
             {
                 slot: 3,
@@ -135,7 +166,9 @@ module.exports = {
                 isDealDamage: false,
                 isBuff: true,
                 power: 1,
-                needlvl: 10
+                needlvl: 10,
+                cost: 800
             }
-        ]}
+        ]
+    }
 };
