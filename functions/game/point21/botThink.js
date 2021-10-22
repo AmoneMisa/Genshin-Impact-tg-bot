654cb55762ee4ee95cc605f899dd59cdb8a857d0
WhitesLove <kubai.rita2@yandex.ru>
2021-10-22 16:36:17 +0300

    Kill players

--- functions/game/point21/botThink.js
+++ functions/game/point21/botThink.js
@@ -2,10 +2,16 @@ const getCard = require('./getCard');
 const getPoints = require('./getPoints');
 
 module.exports = function (chatSession) {
+    let players = Object.entries(chatSession.pointPlayers)
+        .filter(([playerId,]) => playerId !== "bot")
+        .map(([, player]) => player);
+    let points = players.map(player => getPoints(player)).filter(points => points <= 21);
+    let maxPoints = Math.max.apply(null, points) || 0;
+
     while (true) {
        let points = getPoints(chatSession.pointPlayers["bot"]);
 
-       if (points >= 17) {
+       if (points === 21 || points > maxPoints) {
            chatSession.pointPlayers['bot'].isPass = true;
            break;
        }
