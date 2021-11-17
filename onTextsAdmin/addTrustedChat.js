674318eed5e040b428f88b85c9f8eb2f8a18cd6b
WhitesLove <kubai.rita2@yandex.ru>
2021-11-17 20:58:19 +0300

    Release Version 5.2
    Rename trusted chats

--- onTextsAdmin/addTrustedChat.js
+++ onTextsAdmin/addTrustedChat.js
@@ -5,7 +5,7 @@ const {myId} = require('../config');
 const debugMessage = require('../functions/debugMessage');
 const fs = require('fs');
 
-module.exports = [[/(?:^|\s)\/add_chat_as_trusted\b/, async (msg) => {
+module.exports = [[/(?:^|\s)\/mark_trusted\b/, async (msg) => {
     try {
         if (msg.from.id !== myId) {
             return;
@@ -19,12 +19,11 @@ module.exports = [[/(?:^|\s)\/add_chat_as_trusted\b/, async (msg) => {
          return sendMessage(myId, `Чат уже добавлен в доверенные: ${msg.chat.id} - ${msg.chat.title}`);
         }
 
-        fs.writeFileSync(`./trustedChats.json`, JSON.stringify(trustedChats));
+        fs.writeFileSync(`../trustedChats.json`, JSON.stringify(trustedChats));
         sendMessage(myId, `Чат добавлен в доверенные: ${msg.chat.id} - ${msg.chat.title}`);
 
     } catch (e) {
-        debugMessage(`Command: /add_chat_as_trusted\nIn: ${msg.chat.id} - ${msg.chat.title}\n\nError: ${e}`);
+        debugMessage(`Command: /mark_trusted\nIn: ${msg.chat.id} - ${msg.chat.title}\n\nError: ${e}`);
         throw e;
     }
-}
-]];
+}]];
