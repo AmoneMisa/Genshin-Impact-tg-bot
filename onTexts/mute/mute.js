2c46fec532ea527a6b9846a35d0e30dd5aed9cd0
WhitesLove <kubai.rita2@yandex.ru>
2021-12-29 15:21:06 +0300

    Fix for selfmute

--- onTexts/mute/mute.js
+++ onTexts/mute/mute.js
@@ -5,7 +5,6 @@ const sendMessage = require('../../functions/sendMessage');
 module.exports = [[/(?:^|\s)\/self_mute\b/, (msg, session) => {
     try {
         bot.deleteMessage(msg.chat.id, msg.message_id);
-        let success = false;
         bot.restrictChatMember(msg.chat.id, msg.from.id, {
             permissions: {
                 can_send_messages: false,
@@ -15,18 +14,19 @@ module.exports = [[/(?:^|\s)\/self_mute\b/, (msg, session) => {
                 can_pin_messages: false
             },
             until_date: Math.round(new Date().getTime() / 1000.0) + (2 * 60)
-        }).then(res => success = res);
+        }).then(res => {
+            if (res) {
+                sendMessage(msg.chat.id, `@${session.userChatData.user.username}, получил мут на 2 минуты.`, {
+                    disable_notification: true
+                })
+            } else {
+                sendMessage(msg.chat.id, `@${session.userChatData.user.username}, админу нельзя замутаться.`, {
+                    disable_notification: true
+                });
+                debugMessage(`${session.userChatData.user.username},\n\n${JSON.stringify(session.userChatData)},\n\nsuccess - ${success}`);
+            }
+        });
 
-        if (success) {
-            sendMessage(msg.chat.id, `@${session.userChatData.user.username}, получил мут на 2 минуты.`, {
-                disable_notification: true
-            })
-        } else {
-            sendMessage(msg.chat.id, `@${session.userChatData.user.username}, админу нельзя замутаться.`, {
-                disable_notification: true
-            });
-            debugMessage(`${session.userChatData.user.username},\n\n${JSON.stringify(session.userChatData)},\n\nsuccess - ${success}`);
-        }
     } catch (e) {
         debugMessage(`Command: /self_mute\nIn: ${msg.chat.id} - ${msg.chat.title}\n\nError: ${e}`);
         throw e;
