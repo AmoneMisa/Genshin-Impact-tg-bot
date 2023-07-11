d6218d6cf14bf8f22cd69d9dce60dbd78b487934
WhitesLove <kubai-rita5@gmail.com>
2023-07-11 11:58:52 +0300

    Added new game - bowling

--- onTextsAdmin/game/bowling/resetBowlingGame.js
+++ onTextsAdmin/game/bowling/resetBowlingGame.js
@@ -3,10 +3,10 @@ const sendMessageWithDelete = require('../../../functions/tgBotFunctions/sendMes
 const getSession = require('../../../functions/getters/getSession');
 const endGame = require('../../../functions/game/dice/endGame');
 
-module.exports = [[/(?:^|\s)\/reset_dice_game\b/, async (msg) => {
+module.exports = [[/(?:^|\s)\/reset_bowling_game\b/, async (msg) => {
     bot.deleteMessage(msg.chat.id, msg.message_id);
     let session = await getSession(msg.chat.id, msg.from.id);
     endGame(session);
 
-    sendMessageWithDelete(msg.chat.id, `Сессия игры в кубик сброшена.`, {}, 3000);
+    sendMessageWithDelete(msg.chat.id, `Сессия игры в боулинг сброшена.`, {}, 3000);
 }]];
