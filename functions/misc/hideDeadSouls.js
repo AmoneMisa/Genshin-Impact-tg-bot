fb2ab2e0adeb33bb6f7eebfd474ca314a96c35a5
WhitesLove <kubai-rita5@gmail.com>
2023-07-30 16:12:18 +0300

    debug

--- functions/misc/hideDeadSouls.js
+++ functions/misc/hideDeadSouls.js
@@ -1,11 +1,15 @@
 const data = require("../../data");
 const bot = require("../../bot");
+const debugMessage = require("../tgBotFunctions/debugMessage");
 
 module.exports = async function () {
     for (let [chatId, chatSession] of Object.entries(data.sessions)) {
         for (let [userId, session] of Object.entries(chatSession.members)) {
             bot.getChatMember(chatId, parseInt(userId)).then(chatMember => {
+                debugMessage(JSON.stringify(chatMember));
                 session.isHided = chatMember.status === "left" || chatMember.status === "kicked" || chatMember.status === "banned";
+            }).catch(reason => {
+                session.isHided = true;
             });
         }
     }
