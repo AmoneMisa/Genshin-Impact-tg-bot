b9b1cd9e3884a29843b48456b36a93f0e685f934
WhitesLove <kubai-rita5@gmail.com>
2023-07-30 16:07:41 +0300

    debug

--- functions/misc/hideDeadSouls.js
+++ functions/misc/hideDeadSouls.js
@@ -5,20 +5,7 @@ module.exports = async function () {
     for (let [chatId, chatSession] of Object.entries(data.sessions)) {
         for (let [userId, session] of Object.entries(chatSession.members)) {
             bot.getChatMember(chatId, parseInt(userId)).then(chatMember => {
-                if (!chatMember.hasOwnProperty("isHided")) {
-                    session.isHided = false;
-                }
-
-                if (chatMember.status === "left" || chatMember.status === "kicked" || chatMember.status === "banned") {
-                    session.isHided = true;
-                    return;
-                }
-
-                if (chatMember && session.isHided) {
-                    session.isHided = false;
-                } else {
-                    session.isHided = false;
-                }
+                session.isHided = chatMember.status === "left" || chatMember.status === "kicked" || chatMember.status === "banned";
             });
         }
     }
