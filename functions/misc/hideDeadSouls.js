b48a02db7ec51c6067c91d03a078ca5cb29aeb33
WhitesLove <kubai-rita5@gmail.com>
2023-07-30 15:11:46 +0300

    Try to fix

--- functions/misc/hideDeadSouls.js
+++ functions/misc/hideDeadSouls.js
@@ -5,8 +5,7 @@ module.exports = async function () {
     for (let [chatId, chatSession] of Object.entries(data.sessions)) {
         for (let [userId, session] of Object.entries(chatSession.members)) {
             bot.getChatMember(chatId, parseInt(userId)).then(chatMember => {
-
-                if (!chatMember) {
+                if (chatMember.status === "left" || chatMember.status === "kicked" || chatMember.status === "banned") {
                     session.isHided = true;
                     return;
                 }
