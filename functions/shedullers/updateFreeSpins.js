6296b7ff007869af40d3efce3e1950614e7c828b
WhitesLove <kubai-rita5@gmail.com>
2023-08-07 00:58:16 +0300

    Fixed cron usage;

--- functions/shedullers/updateFreeSpins.js
+++ functions/shedullers/updateFreeSpins.js
@@ -1,19 +1,16 @@
-const cron = require("node-cron");
 const {sessions} = require("../../data");
 const gachaTemplate = require("../../templates/gachaTemplate");
 
 module.exports = function () {
-    cron.schedule('59 23 * * *', async () => {
-        for (let chatSession of Object.values(sessions)) {
-            for (let session of Object.values(chatSession.members)) {
-                if (session.userChatData.user.is_bot) {
-                    continue;
-                }
+    for (let chatSession of Object.values(sessions)) {
+        for (let session of Object.values(chatSession.members)) {
+            if (session.userChatData.user.is_bot) {
+                continue;
+            }
 
-                for (let value of gachaTemplate) {
-                    session.game.gacha[value.name] = value.freeSpins;
-                }
+            for (let value of gachaTemplate) {
+                session.game.gacha[value.name] = value.freeSpins;
             }
         }
-    });
+    }
 }
