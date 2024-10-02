dbbfaa5b0862734eca120f4074bf90894fb26a3a
WhitesLove <kubai.rita5@gmail.com>
2024-10-02 12:05:37 +0300

    log

--- functions/tgBotFunctions/sendMessage.js
+++ functions/tgBotFunctions/sendMessage.js
@@ -1,5 +1,6 @@
 import retryBotRequest from './retryBotRequest.js';
 
 export default function (chatId, text, form) {
+    console.log("sendMessage", chatId, text);
     return retryBotRequest(bot => bot.sendMessage(chatId, text, form));
 };
