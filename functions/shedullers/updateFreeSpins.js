19c1256622af937af01e167e479c96e771a91df7
MARHARYTA KUBAI <44707323+AmoneMisa@users.noreply.github.com>
2026-09-04 14:52:43 +0300

    fix: reset gacha free spins on normalized array state

--- functions/shedullers/updateFreeSpins.js
+++ functions/shedullers/updateFreeSpins.js
@@ -1,15 +1,18 @@
 import { sessions } from '../../data.js';
 import gachaTemplate from '../../template/gachaTemplate.js';
+import { ensureGachaEntry, normalizeGachaState } from '../game/equipment/normalizeGachaState.js';
 
 export default function () {
     for (let chatSession of Object.values(sessions)) {
         for (let session of Object.values(chatSession.members)) {
-            if (session.userChatData.user.is_bot) {
+            if (session.userChatData?.user?.is_bot) {
                 continue;
             }
 
+            normalizeGachaState(session);
             for (let value of gachaTemplate) {
-                session.game.gacha[value.name] = value.freeSpins;
+                let entry = ensureGachaEntry(session, value.name);
+                entry.freeSpins = value.freeSpins;
             }
         }
     }
