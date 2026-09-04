ba7c8c4cc441ae8b23abc7768f413cc3c053d2ca
MARHARYTA KUBAI <44707323+AmoneMisa@users.noreply.github.com>
2026-09-04 18:21:19 +0300

    fix: await dice owner check

--- callbacks/game/dice/pullDice.js
+++ callbacks/game/dice/pullDice.js
@@ -12,7 +12,7 @@ import loadPlayer from '../../../functions/getters/loadPlayer.js';
 let maxPulls = 3;
 
 export default [[/^dice_pull$/, async function (session, callback) {
-    if (!checkUserCall(callback, session)) {
+    if (!await checkUserCall(callback, session)) {
         return ;
     }
 
